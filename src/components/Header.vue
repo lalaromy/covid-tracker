566fee8bbb1861f79ee08b80f4a7e4f7519464b9
romyabbrederis <romy.abbrederis@gmail.com>
2021-06-18 22:36:11 +0200

    inital commit

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header class="text-center bg-blue-800 text-white p-4 mb-10 ">
+      <div class="text-3xl md:text-5xl font-bold mb-3">
+        <i class="fa fa-viruses"></i>Covid Tracker
+      </div>
+      <p>
+        API by
+        <a class="text-blue-300" href="https://covid19api.com" target="_blank"
+          >covid19api.com</a
+        >
+      </p>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
